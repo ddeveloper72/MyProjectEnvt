fcbea60909c478ed1c13a64c61a0d3e87501d934
ddeveloper72 <duncan.niamh@gmail.com>
2022-12-02 21:23:22 +0000

    added import for environ

--- MyNoSQLProj/settings.py
+++ MyNoSQLProj/settings.py
@@ -10,6 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# Import environmental variables
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
