9968df383b7e39ba11c5315f6d1acb7c8eb2510d
ddeveloper72 <duncan.niamh@gmail.com>
2023-02-04 18:34:00 +0000

    created get tasks url path

--- NoSQLCRUD/urls.py
+++ NoSQLCRUD/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('',views.index,name='index'),]
+urlpatterns = [
+    path('', views.view_all_tasks, name='get_tasks')
+    # path('', views.add_task, name='add_task'),
+    
+    ]
