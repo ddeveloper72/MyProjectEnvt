68120e33726179011ea12989737ce771d402ef6e
Duncan Falconer <duncan.niamh@gmail.com>
2023-02-24 18:32:09 +0000

    added path for edit_task by id

--- NoSQLCRUD/urls.py
+++ NoSQLCRUD/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_all_tasks, name='get_tasks')
-    # path('', views.add_task, name='add_task'),
-    
-    ]
+    path('', views.view_all_tasks, name='get_tasks'),
+    path('edit_task/<task_id>', views.edit_task, name='edit_task'),
+    # path('insert_task/<task_id>', views.insert_task, name='insert_task')
+
+]
