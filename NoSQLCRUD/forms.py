469aa04d9172bbb6f077d8cd66e82e9012c8d1fd
Duncan Falconer <duncan.niamh@gmail.com>
2023-03-14 18:20:54 +0000

    added an insert task form for form validation

---
+++ NoSQLCRUD/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+# add a new task from
+class InsertTaskForm(forms.Form):
+    task_name = forms.CharField(max_length=50, required=True)
+    category_name = forms.CharField(max_length=50, required=True)
+    task_description = forms.CharField(widget=forms.Textarea, required=True)
+    due_date = forms.DateField(required=True)
+    is_urgent = forms.BooleanField(required=False)
