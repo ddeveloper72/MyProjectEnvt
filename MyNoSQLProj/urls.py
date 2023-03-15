7ba561f9625062aa573570b05a17b225258486d1
Duncan Falconer <duncan.niamh@gmail.com>
2023-03-15 10:51:48 +0000

    redirect the root URL to the task_manager URL

--- MyNoSQLProj/urls.py
+++ MyNoSQLProj/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
+
+# redirect the root URL to the task_manager URL
 urlpatterns = [
-    path('noSQL_CRUD/', include('NoSQLCRUD.urls')),
+    path('', RedirectView.as_view(url='task_manager/', permanent=True)),
+    path('task_manager/', include('NoSQLCRUD.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
