11911bf54c71d4d4e6c868abbbeac6f53e4e31ef
Duncan Falconer <duncan.niamh@gmail.com>
2023-03-15 12:45:49 +0000

    add a new category from

--- NoSQLCRUD/forms.py
+++ NoSQLCRUD/forms.py
@@ -8,3 +8,9 @@ class InsertTaskForm(forms.Form):
     task_description = forms.CharField(widget=forms.Textarea, required=True)
     due_date = forms.DateField(required=True)
     is_urgent = forms.BooleanField(required=False)
+
+
+# add a new category from
+class InsertCategoryForm(forms.Form):
+    category_name = forms.CharField(max_length=50, required=True)
+    category_description = forms.CharField(widget=forms.Textarea, required=True)
